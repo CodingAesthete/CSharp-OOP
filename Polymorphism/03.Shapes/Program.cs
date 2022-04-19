572cf62557bec209afea60856bc72d0b34a171bf
PostPunkAesthete <harry_ollI@abv.bg>
2022-04-19 11:40:25 +0300

    Initial commit

---
+++ Polymorphism/03.Shapes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(3,4);
+            Shape circle = new Circle(6);
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.Draw());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
